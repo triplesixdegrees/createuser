dcdbcd1f499313fe37bb24ba3c4fb70dbb2351f4
triplesixdegrees <103490220+triplesixdegrees@users.noreply.github.com>
2022-04-12 11:18:55 +0300

    Update createuser.sh

--- createuser.sh
+++ createuser.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-userfile=/userlist 
-username=$(cat /tmp/userlist | tr 'A-Z'  'a-z')
+userfile=userlist 
+username=$(cat userlist | tr 'A-Z'  'a-z')
 password=$username@123
 for user in $username
 do
        useradd $user
        echo $password | passwd --stdin $user
 done
-echo "$(wc -l /tmp/userlist) users have been created" 
-tail -n$(wc -l /tmp userlist) /etc/passwd
+echo "$(wc -l userlist) users have been created" 
+tail -n$(wc -l userlist) /etc/passwd
